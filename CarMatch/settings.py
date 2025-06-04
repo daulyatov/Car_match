211530b5c82417956f448a7679f415fd162d2d16
dltv19 <sultandaulatov@gmail.com>
2025-06-05 01:01:16 +0500

    ALLOWED_HOSTS

--- CarMatch/settings.py
+++ CarMatch/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = ['carmatch.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
